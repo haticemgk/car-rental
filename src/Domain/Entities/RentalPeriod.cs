7e7a1f62f01593fd4441a62ef6f9b77cd550c0f6
haticemgk <80325516+haticemgk@users.noreply.github.com>
2021-04-16 14:50:34 +0300

    Creating persistence layer

---
+++ src/Domain/Entities/RentalPeriod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class RentalPeriod
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength]
+        public string Name { get; set; }
+
+    }
+}
