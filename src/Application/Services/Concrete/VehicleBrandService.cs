99501765c29435902745aea0de1da4b14c520044
haticemgk <80325516+haticemgk@users.noreply.github.com>
2021-04-21 01:55:47 +0300

    Adding Response class Adding validation to vehicle brand adding operation

---
+++ src/Application/Services/Concrete/VehicleBrandService.cs
@@ -0,0 +1,60 @@
+﻿using Application.Infrastructure.Persistence;
+using Domain.DTOs;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Services.Concrete
+{
+    public class VehicleBrandService : IVehicleBrandService
+    {
+        private ICarRentalDbContext Context { get; }
+        public VehicleBrandService(ICarRentalDbContext context)
+        {
+            Context = context;
+        }
+
+        public Response Add(VehicleBrand vehicleBrand)
+        {
+            int sameNumberOfRecords = Context.VehicleBrand.Where(v => v.Name == vehicleBrand.Name).Count();
+            if(sameNumberOfRecords > 0)
+            {
+                return Response.Fail($"{vehicleBrand.Name} markası sistemde zaten kayıtlıdır.");
+            }
+
+            Context.VehicleBrand.Add(vehicleBrand);
+            Context.SaveChanges();
+
+            return Response.Success("Marka başarıyla kaydedildi");
+        }
+        public void Update(VehicleBrand vehicleBrand)
+        {
+            var vehicleBrandToUpdate = GetById(vehicleBrand.Id);
+            vehicleBrandToUpdate.Name = vehicleBrand.Name;
+            Context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var vehicleBrandToDelete = GetById(id);
+            Context.VehicleBrand.Remove(vehicleBrandToDelete);
+            Context.SaveChanges();
+        }
+
+        public VehicleBrand GetById(int id)
+        {
+            return Context.VehicleBrand.Where(v => v.Id == id).SingleOrDefault();
+        }
+        public List<VehicleBrand> Get(VehicleBrandFilter filter)
+        {
+            var items = (from v in Context.VehicleBrand
+                         where v.Name.StartsWith(filter.Name)
+                         orderby v.Name
+                         select v).ToList();
+            return items;
+        }
+        public string GetName()
+        {
+            return "Vehicle brand service";
+        }
+    }
+}
--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,30 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
-    {
+    {//IOC = Inversion of Control
+        //Dependency Injection
         private readonly ILogger<HomeController> _logger;
+        private readonly IVehicleBrandService _service;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IVehicleBrandService vehicleBrandService)
         {
             _logger = logger;
+            _service = vehicleBrandService;
         }
 
         public IActionResult Index()
         {
+            
+            string serviceName = _service.GetName();
+            ViewBag.ServiceName = serviceName;
             return View();
         }
 
         public IActionResult Privacy()
         {
+            
+            string serviceName = _service.GetName();
+            ViewBag.ServiceName = serviceName;
             return View();
         }
 
